326d142ec358b221c2250025639ac557e6712f51
Murphy_Law <belmontemurphy@gmail.com>
2025-09-06 23:03:27 +0600

    adding Tourism Folder correctly

--- obj/Debug/net9.0/TourismApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/TourismApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TourismApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57e547fedde98dae7db1206f1f6c28053b890f0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54670fbf56f1c5e1bbf9edb34f9918a0e54da0f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TourismApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TourismApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
